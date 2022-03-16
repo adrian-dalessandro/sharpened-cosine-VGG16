39ae6cb367fe170aaa329316298771346599fa7c
acdaless <acdaless@cs-mial-59.cmpt.sfu.ca>
2022-03-15 18:25:39 -0700

    introduced a working efficient version of the CosSimConv2d, a lazy version of MaxAbsPool, and a demo notebook

--- src/custom_layers.py
+++ src/custom_layers.py
@@ -17,9 +17,15 @@ class FasterCosSimConv2D(tf.keras.layers.Layer):
         self.bias_initializer = bias_initializer
 
     def build(self, input_shape):
-        self.conv = tf.keras.layers.Conv2D(self.filters, self.kernel_size, strides=self.strides, padding=self.padding,
-                 dilation_rate=self.dilation_rate, activation=self.activation, use_bias=self.use_bias, kernel_initializer=self.kernel_initializer,
-                 bias_initializer=self.bias_initializer)
+        self.conv = tf.keras.layers.Conv2D(self.filters, 
+                                           self.kernel_size, 
+                                           strides=self.strides, 
+                                           padding=self.padding,
+                                           dilation_rate=self.dilation_rate, 
+                                           activation=self.activation, 
+                                           use_bias=self.use_bias,
+                                           kernel_initializer=self.kernel_initializer,
+                                           bias_initializer=self.bias_initializer)
         
         self.p = self.add_weight(
             shape=(self.filters,), initializer='zeros', trainable=True)
@@ -29,7 +35,13 @@ class FasterCosSimConv2D(tf.keras.layers.Layer):
 
 
     def call(self, inputs, training=None):
-        
+        """ tf.images.extract_patches grabs patches from an input as if a sliding window
+            has been passed over the input. Given that the numerator term in the sharpened
+            cosine similarity formula is essentially a convolution, it is more efficient in
+            Tensorflow to rely on the default convolution operator for the numerator. This 
+            leaves the problem of calculating the L2-norm for the image patches. However, calling
+            extract_patches against the input with the same arguments as the Conv2D layer gives us
+            the necessary image patches."""
         x_patches = tf.image.extract_patches(images=inputs,
                                        sizes=[1, self.kernel_size, self.kernel_size, 1],
                                        strides=[1, self.strides, self.strides, 1],
@@ -39,14 +51,27 @@ class FasterCosSimConv2D(tf.keras.layers.Layer):
         
         x_feats = self.conv(inputs)
         q = tf.square(self.q) / 10
-        x_norm = tf.math.reduce_euclidean_norm(x_patches, axis=-1)[:,:,:,None] + q
-        w_norm = tf.math.reduce_euclidean_norm(self.conv.weights[0], axis=(0,1,2))[None,None,None,:] + q
-        
+        x_norm = tf.sqrt(tf.maximum(tf.math.reduce_sum(tf.square(x_patches), axis=-1), 1e-12))[:,:,:,None] + q
+        w_norm = tf.sqrt(tf.maximum(tf.math.reduce_sum(tf.square(self.conv.weights[0]),  
+                                                       axis=(0,1,2)), 1e-12))[None,None,None,:] + q        
         x = x_feats/tf.multiply(x_norm, w_norm)
         x = tf.abs(x) + 1e-12
         
         sign = tf.sign(x_feats)
-        x = tf.pow(x, tf.square(self.p)/100)
+        x = tf.pow(x, tf.nn.softmax(self.p))
         x = sign * x
-        
         return x
+
+
+class MaxAbsPool2D(tf.keras.layers.Layer):
+    "Inefficient and lazy version of MaxAbsPool2D which pools twice, but it's much shorter to write."""
+    def __init__(self, pool_size):
+        super(MaxAbsPool2D, self).__init__()
+        self.pooling = tf.keras.layers.MaxPool2D(pool_size=pool_size)
+        
+    def call(self, inputs, training=None):
+        pos_pool = self.pooling(inputs)
+        neg_pool = self.pooling(-1*inputs)
+        
+        mask = tf.cast(neg_pool > pos_pool, dtype=tf.float32)
+        return -1*mask*neg_pool + (1-mask)*pos_pool
